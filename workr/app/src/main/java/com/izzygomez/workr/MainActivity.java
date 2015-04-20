0fe91338d451fae9781ba7a0f02a7a44a37126d2
Jordan <powellj@mit.edu>
2015-04-20 02:25:35 -0400

    Adding and deleting implemented

--- workr/app/src/main/java/com/izzygomez/workr/MainActivity.java
+++ workr/app/src/main/java/com/izzygomez/workr/MainActivity.java
@@ -2,18 +2,49 @@ package com.izzygomez.workr;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    boolean deleteMode = false;
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        ListView taskListView = (ListView) findViewById(R.id.listViewOfTasks);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
 
+        adapter.notifyDataSetChanged();
+        Log.d("listItems",listItems.toString());
+        Toast.makeText(this, listItems.toString(), Toast.LENGTH_LONG).show();
+        taskListView.setAdapter(adapter);
+        taskListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                if (deleteMode){
+                    arg0.getItemAtPosition(position);
+                    listItems.remove(position);
+                    adapter.notifyDataSetChanged();
+                }
+//            arg0.getItemAtPosition(position);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +66,32 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addToList(List<String> taskInputs){
+        String listViewInput = "";
+        for (String input: taskInputs){
+            listViewInput += input + " ";
+        }
+        listItems.add(listViewInput);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void addToListTest(View v){
+        List<String> taskInputs = new ArrayList<String>();
+        taskInputs.add("This");
+        taskInputs.add("is");
+        taskInputs.add("a");
+        taskInputs.add("test");
+        addToList(taskInputs);
+    }
+
+    public void setDeleteMode(View v){
+        if (deleteMode){
+            deleteMode = false;
+        }
+        else{
+            deleteMode = true;
+        }
     }
 }
